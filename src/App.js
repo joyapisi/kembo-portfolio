ca421b8cceb134d454d46ab07800a00876327200
Joy Phoebe <joyapisi@gmail.com>
2023-10-06 17:38:04 +0300

    Add components to App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React.</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <main className="text-gray-400 bg-gray-900 body-font">
+        <Navbar />
+        <About />
+        <Projects />
+        <Contact />
+     
+        <div className="App">
+            <div className="App-header">
+              <h2>Welcome to React.</h2>
+            </div>
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+        </div> 
+      </main>
     );
   }
 }
