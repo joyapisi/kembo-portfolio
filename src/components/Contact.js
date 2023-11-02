2361e5113dadf437c7b54343fb2f9ef9c2373deb
Joy Phoebe <joyapisi@gmail.com>
2023-11-02 17:20:01 +0300

    Fix template ID to allow contact form to work well

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,38 +1,65 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import emailjs from '@emailjs/browser';
 
 export default function Contact() {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+  const [loading, setLoading] = useState(false);
 
-    const [name, setName] = React.useState("");
-    const [email, setEmail] = React.useState("");
-    const [message, setMessage] = React.useState("");
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
 
-    function encode(data) {
-        return Object.keys(data)
-          .map(
-            (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-          )
-          .join("&");
-      }
-    
-      function handleSubmit(e) {
-        e.preventDefault();
-        fetch("/", {
-          method: "POST",
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: encode({ "form-name": "contact", name, email, message }),
-        })
-          .then(() => alert("Message sent!"))
-          .catch((error) => alert(error));
-      }
+  function handleSubmit(e) {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        'service_hcmr0wh',
+        'contact_form',
+        {
+          from_name: form.name,
+          to_name: 'Walter',
+          from_email: form.email,
+          to_email: 'walterkembo@gmail.com',
+          message: form.message,
+        },
+        'uNQBJSv5uFmYe2PGG'
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert('Thanks for contacting me! I will get back to you as soon as possible.');
+
+          setForm({
+            name: '',
+            email: '',
+            message: '',
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.error('Error sending email:', error);
+          alert('Something went wrong. Please try again.');
+        }
+      );
+  }
 
   return (
     <section id="contact" className="relative">
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
         <form
-          netlify
+          ref={formRef}
           name="contact"
           onSubmit={handleSubmit}
-          className="lg:w-full md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          className="lg:w-full md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
+        >
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Hire Me
           </h2>
@@ -45,10 +72,10 @@ export default function Contact() {
             </label>
             <input
               type="text"
-              id="name"
               name="name"
+              value={form.name}
+              onChange={handleChange}
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div className="relative mb-4">
@@ -57,10 +84,10 @@ export default function Contact() {
             </label>
             <input
               type="email"
-              id="email"
               name="email"
+              value={form.email}
+              onChange={handleChange}
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="relative mb-4">
@@ -70,16 +97,18 @@ export default function Contact() {
               Message
             </label>
             <textarea
-              id="message"
+              rows="7"
               name="message"
+              value={form.message}
+              onChange={handleChange}
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setMessage(e.target.value)}
             />
           </div>
           <button
             type="submit"
-            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-            Submit
+            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+          >
+            {loading ? 'Sending' : 'Send'}
           </button>
         </form>
       </div>
