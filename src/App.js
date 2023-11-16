0d2a1ec780db5305985a06339d516992e172beef
Joy Phoebe <joyapisi@gmail.com>
2023-11-16 12:35:34 +0300

    Rename ProjectCards to Cards

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import ProjectList from "./components/ProjectList";
+import Cards from "./components/Cards";
 
 export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
      <Navbar />
      <About />
+     <Cards />
      <ProjectList />
      <Contact />
      </main>
