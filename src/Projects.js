5f62b56dc7611c44b3fac05e8ac6178055bed4eb
Joy Phoebe <joyapisi@gmail.com>
2023-11-16 12:44:25 +0300

    Change link for project video

--- src/Projects.js
+++ src/Projects.js
@@ -1,6 +1,6 @@
 export const Projects = [
   {
     thumbnail: "./reel.gif",
-    url: "https://drive.google.com/file/d/1IrTlabVeBVvxBP7ZCLU-bvmDLjnJnAhf/view?usp=share_link",
+    url: "https://www.youtube.com/watch?v=sjoldEFX__c",
   }
 ];
