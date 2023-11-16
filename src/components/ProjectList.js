4c455fa53fd6eb3f8a3425c0930e810a9d3f2c9a
Joy Phoebe <joyapisi@gmail.com>
2023-11-16 12:40:28 +0300

    Add ProjectList

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -16,6 +16,26 @@ export default function ProjectList() {
           </p>
           <br className="hidden lg:inline-block" />
         </div>
+
+        <div className="cont s--inactive">
+          <div className="cont__inner">
+            <div className="el__overflow">
+              <div className="el__inner">
+                <div className="el__bg"></div>
+                <div className="el__preview-cont">
+                  <h2 className="el__heading">
+                    Section 1 
+                  </h2>
+                </div>
+                <div className="el__content">
+                  <div className="el__text">yyyyy</div>
+                  <div className="el__close-btn">
+                   </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
                     
         <div className="flex flex-wrap -m-4">
           {Projects.map((video, index) => (
